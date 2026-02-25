55e241ef7f6496a2b53d96823db234eb835b4ee0
Rocospar <alexandrucostin111@gmail.com>
2026-02-26 01:45:04 +0200

    Add tokenizing for the command string

--- main.c
+++ main.c
@@ -2,14 +2,44 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 void check_command(char *commandstr,int *loop)
 {
-  if(strcmp(commandstr,"exit")==0)
-   *loop=0;
-  else if(strncmp(commandstr,"echo ",5)==0)
-  printf("%s\n",commandstr+5);
-  else printf("%s command not found\n",commandstr); 
+  char *tok;
+  tok=strtok(commandstr," ");
+
+  if(tok==NULL) return;
+
+  if(strcmp(tok,"exit") ==0 )
+  {
+    *loop=0;
+  }
+  else if(strcmp(tok,"echo") ==0 )
+  {
+    while(tok)
+    {
+        tok=strtok(NULL," ");
+        if(tok!=NULL)
+        printf("%s ",tok);
+        
+    }
+    printf("\n");
+    
+  }
+  else if(strcmp(tok,"type") ==0 )
+  {
+    tok=strtok(NULL," ");
+    if(tok==NULL) return;
+    if(strcmp(tok,"echo") == 0 || strcmp(tok,"type") == 0 || strcmp(tok,"exit") == 0)
+      printf("%s is a shell builtin\n",tok);
+    else 
+      printf("%s: not found\n",tok);
+
+   }
+
+   else printf("%s: command not found\n",tok);
 }
+
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -21,7 +51,8 @@ int main(int argc, char *argv[]) {
   {
     printf("$ ");
     fgets(command, sizeof(command), stdin);
-    command[strcspn(command, "\n")] = 0;
+    //Eliminated "\0" from the string
+    command[strcspn(command, "\n")] = 0;  
 
     check_command(command,&loop);
 
@@ -30,3 +61,4 @@ int main(int argc, char *argv[]) {
  
 
 }
+
